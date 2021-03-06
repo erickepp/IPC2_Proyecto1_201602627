3da801f4973c6ecdb096173e930fb1528e3b699c
Erick Patzan <erickestuardopatzan@gmail.com>
2021-03-05 20:49:09 -0600

    Agregando función para validar el nombre de una matriz

--- esquema_xml.py
+++ esquema_xml.py
@@ -1,6 +1,16 @@
+from lista_circular import ListaCircular
+
 def validar_etiqueta_matriz(elem, nombre):
     if elem.tag != 'matriz':
         print(f'Error: <{elem.tag} nombre="{nombre}">')
         print('La ubicación de la etiqueta no es válida\n')
         return False
     return True
+
+def validar_nombre_matriz(matrices, nombre):
+    for matriz in matrices:
+        if matriz.nombre == nombre:
+            print(f'Error: <matriz nombre="{nombre}">')
+            print('La matriz ya existe\n')
+            return False
+        return True
